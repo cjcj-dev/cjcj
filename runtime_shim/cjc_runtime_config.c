b0d6ffdb424af6758e7e62c0f2701c1d6b7aa76f
Zxilly <zxilly@outlook.com>
2026-07-17 09:53:04 +0000

    fix(cjc): mirror macro stack environment validation

--- runtime_shim/cjc_runtime_config.c
+++ runtime_shim/cjc_runtime_config.c
@@ -4,6 +4,79 @@
  * compiler must apply the same default before its process runtime starts.
  */
 #include <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#define UNIT_LEN 2
+#define KB 1024
+#define MB (KB * KB)
+#define CO_STACK_SIZE (4 * KB)
+#define G_MIN_STACK_SIZE 64
+#define G_MAX_STACK_SIZE (1UL * MB)
+
+static size_t GetSizeFromEnv(const char* value)
+{
+    char compact[64];
+    size_t src = 0;
+    size_t dst = 0;
+    while (value[src] != '\0') {
+        if (!isspace((unsigned char)value[src])) {
+            if (dst + 1 >= sizeof(compact)) {
+                return SIZE_MAX;
+            }
+            compact[dst++] = value[src];
+        }
+        ++src;
+    }
+    compact[dst] = '\0';
+    if (dst <= UNIT_LEN) {
+        return SIZE_MAX;
+    }
+
+    char* unit = compact + dst - UNIT_LEN;
+    unit[0] = (char)tolower((unsigned char)unit[0]);
+    unit[1] = (char)tolower((unsigned char)unit[1]);
+    if (strcmp(unit, "kb") != 0 && strcmp(unit, "mb") != 0 && strcmp(unit, "gb") != 0) {
+        return SIZE_MAX;
+    }
+    char unitKind = unit[0];
+    *unit = '\0';
+
+    char* end = NULL;
+    long number = strtol(compact, &end, 10);
+    if (end == compact || *end != '\0' || number > INT32_MAX) {
+        return SIZE_MAX;
+    }
+    if (number <= 0) {
+        return 0;
+    }
+    if (unitKind == 'm') {
+        return (size_t)number * KB;
+    }
+    if (unitKind == 'g') {
+        return (size_t)number * MB;
+    }
+    return (size_t)number;
+}
+
+static size_t GetStackSizeFromEnv(const char* name)
+{
+    const char* value = getenv(name);
+    if (value == NULL) {
+        return CO_STACK_SIZE;
+    }
+    size_t stackSize = GetSizeFromEnv(value);
+    if (stackSize == SIZE_MAX) {
+        return CO_STACK_SIZE;
+    }
+    if (stackSize < G_MIN_STACK_SIZE || stackSize > G_MAX_STACK_SIZE) {
+        return CO_STACK_SIZE;
+    }
+    return stackSize;
+}
 
 #ifdef _WIN32
 static void __cdecl CallRuntime(void);
@@ -12,15 +85,15 @@ __declspec(allocate(".CRT$XCU")) void(__cdecl* CallRuntimeInitializer)(void) = C
 
 static void __cdecl CallRuntime(void)
 {
-    if (getenv("CJSTACKSIZE") == NULL) {
-        (void)_putenv_s("CJSTACKSIZE", "4mb");
-    }
+    char stackSize[32];
+    (void)snprintf(stackSize, sizeof(stackSize), "%zukb", GetStackSizeFromEnv("CJSTACKSIZE"));
+    (void)_putenv_s("CJSTACKSIZE", stackSize);
 }
 #else
 __attribute__((constructor)) static void CallRuntime(void)
 {
-    if (getenv("cjStackSize") == NULL) {
-        (void)setenv("cjStackSize", "4mb", 0);
-    }
+    char stackSize[32];
+    (void)snprintf(stackSize, sizeof(stackSize), "%zukb", GetStackSizeFromEnv("cjStackSize"));
+    (void)setenv("cjStackSize", stackSize, 1);
 }
 #endif
